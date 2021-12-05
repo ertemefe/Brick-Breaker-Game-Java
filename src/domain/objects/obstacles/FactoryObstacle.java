3b9b10d821ad6a2754be3a088e825ed8dd11baa5
efe <eertem19@ku.edu.tr>
2021-12-05 18:56:14 +0300

    further changes

--- src/domain/objects/obstacles/FactoryObstacle.java
+++ src/domain/objects/obstacles/FactoryObstacle.java
@@ -2,19 +2,35 @@ package domain.objects.obstacles;
 
 public class FactoryObstacle {
 
-    public Obstacle createObstacle(){
+    private static FactoryObstacle instance;
 
-        Obstacle newObstacle = null;
+    private FactoryObstacle() {
+    }
+
+    public static FactoryObstacle getInstance() {
+        if (instance == null) {
+            instance = new FactoryObstacle();
+        }
+        return instance;
+    }
+
+    public Obstacle createObstacle(String type) {
 
         /*
         buraya böyle bilmem ne text field seçiliyse ve yeni sayı eski sayıdan farklıysa (?)
         yeni sayı kadar object oluştur denmeli
         yada
         */
-
         //ama mesela sayı küçülürse en son eklenen obstacle silinmeli
         //obstacleları min sayıdan başlatalım alt sınırdan aşağı geçmesini engelleyelim
 
-        return null;
+        return switch (type) {
+            case "simple" -> new ObstacleSimple();
+            case "firm" -> new ObstacleFirm();
+            case "explosive" -> new ObstacleExplosive();
+            case "gift" -> new ObstacleGift();
+            default -> null;
+        };
+
     }
 }
