055e0b84edac37efc21bc034e45483d81fb07587
CELAL KAPLAN <ckaplan19@ku.edu.tr>
2021-12-31 01:47:36 +0300

    Add Ymir

---
+++ src/domain/objects/Ymir.java
@@ -0,0 +1,69 @@
+package domain.objects;
+
+import domain.Controller;
+import domain.objects.obstacles.Obstacle;
+
+import java.util.Random;
+
+public class Ymir {
+
+    private static final int FROZEN_TIME = 15000;
+    private static Ymir instance;
+    private Controller controller = Controller.getInstance();
+    private Random random;
+
+    private final int PERIOD;
+    private int remainingTime;
+
+    private Ymir(int period) {
+        PERIOD = period;
+        remainingTime = PERIOD;
+        random = new Random();
+    }
+
+    public static Ymir getInstance(int period) {
+        if(instance==null)
+            instance = new Ymir(period);
+        return instance;
+    }
+
+    public void updateRemainingTime(int millisecond) {
+        remainingTime -= millisecond;
+        if(remainingTime <= 0) {
+            doAction();
+            remainingTime = PERIOD;
+        }
+    }
+
+    private void doAction() {
+        if(random.nextBoolean())    // %50 probability
+            return;
+        int option = random.nextInt(3);  // 0: infinite void, 1: time alter, 2: hollow purple
+        switch (option) {
+            case 0:
+                infiniteVoid();
+                break;
+            case 1:
+                doubleAccel();
+                break;
+            case 2:
+                hollowPurple();
+                break;
+        }
+
+    }
+
+    private void infiniteVoid() {
+        for(int i = 0 ; i<8 ; i++) {
+            Object[] values = controller.obstacles.values().toArray();
+            Obstacle randomObstacle = (Obstacle) values[random.nextInt(values.length)];
+            randomObstacle.startFrozen(FROZEN_TIME);
+        }
+    }
+
+    private void doubleAccel() {
+    }
+
+    private void hollowPurple() {
+    }
+}
